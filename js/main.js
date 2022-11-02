4f328d4bafe81b3f58ca9a2b6000cc2031309a43
keyztrokee <raymondfajiculay.dev@gmail.com>
2022-11-02 13:05:56 +0800

    post only

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/*--------------------------------------------------/*
+ * Nav Bar
+/*--------------------------------------------------*/
 const hamburger = document.querySelector(".hamburger ");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -12,3 +15,35 @@ document.querySelectorAll(".nav-item").forEach((n) =>
         navMenu.classList.remove("active");
     })
 );
+
+/*--------------------------------------------------/*
+ * Accordion
+/*--------------------------------------------------*/
+
+const contentBox = document.querySelectorAll(".contentBox");
+const content = document.querySelectorAll(".content");
+
+contentBox.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        content.forEach((acc) => {
+            if (acc.classList.contains("active")) {
+                acc.classList.remove("active");
+                contentBox.forEach((btn) => {
+                    btn.classList.remove("active");
+                    btn.firstElementChild.firstElementChild.classList.remove(
+                        "active"
+                    );
+                });
+            }
+        });
+
+        const panel = btn.lastElementChild;
+        panel.classList.toggle("active");
+        btn.classList.toggle("active");
+        console.log(panel);
+
+        const icon = btn.firstElementChild.firstElementChild;
+        icon.classList.toggle("active");
+        console.log(icon);
+    });
+});
